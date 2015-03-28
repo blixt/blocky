473b28afa7bfb0cee323a6cd559037764e543b85
Blixt <me@blixt.org>
2015-03-28 16:35:52 -0400

    Use port 8080 and add logging

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
-	"log"
 	"net/http"
+	"os"
 
+	"github.com/op/go-logging"
 	"golang.org/x/net/websocket"
 )
+
+var log = logging.MustGetLogger("blocky")
+
+const logFormat = "%{time:15:04:05.000} %{level:.4s} [%{shortfunc}] %{message}"
 
 func client(ws *websocket.Conn) {
 }
@@ -14,8 +19,13 @@ func region(id string) {
 }
 
 func main() {
+	// Set up logging.
+	backend := logging.NewLogBackend(os.Stderr, "", 0)
+	formatter := logging.NewBackendFormatter(backend, logging.MustStringFormatter(logFormat))
+	logging.SetBackend(formatter)
 
 	// Start the websocket server.
+	log.Info("Listening on port 8080...")
 	http.Handle("/socket", websocket.Handler(client))
-	log.Fatal(http.ListenAndServe(":12345", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
