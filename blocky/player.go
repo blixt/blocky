cb7272b99a2df19d97be03ea4b60195dc732e6d4
Blixt <me@blixt.org>
2015-03-30 22:23:08 -0400

    Player's get their move on

--- blocky/player.go
+++ blocky/player.go
@@ -1,6 +1,7 @@
 package blocky
 
 import (
+	"errors"
 	"time"
 )
 
@@ -9,6 +10,8 @@ type Player struct {
 	Name     string
 	Ping     float64
 	PingTime time.Time
+	X, Y     int
+	lastMove time.Time
 }
 
 var players = make(map[Id]*Player)
@@ -21,4 +24,18 @@ func NewPlayer() *Player {
 	player := &Player{Id: NewId(), Name: "Guest"}
 	players[player.Id] = player
 	return player
+}
+
+func (p *Player) Move(dx, dy int) error {
+	if dx < -1 || dx > 1 || dy < -1 || dy > 1 {
+		return errors.New("Invalid move delta")
+	} else if dx != 0 && dy != 0 {
+		return errors.New("Can only move in one direction at a time")
+	} else if time.Now().Sub(p.lastMove) < 100*time.Millisecond {
+		return errors.New("Can only move once every 100 ms")
+	}
+	p.X += dx
+	p.Y += dy
+	p.lastMove = time.Now()
+	return nil
 }
